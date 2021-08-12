810057903827596d35ee33546dbb825d5f22fd4a
Lucas Puorto <lucas.puorto@br.experian.com>
2021-08-11 22:50:34 -0300

    Logout control in activity, if a user is unauthenticated

--- app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,21 +1,38 @@
 package com.udacity.project4.locationreminders
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
 import kotlinx.android.synthetic.main.activity_reminders.*
+import org.koin.android.ext.android.inject
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
 
+    private val viewModel: ReminderViewModel by inject()
+
+    //Logout control in activity. If a user is unauthenticated, It is not possible to use any feature. 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reminders)
-
+        viewModel.authenticationState.observe(this, { authenticationState ->
+            when (authenticationState) {
+                ReminderViewModel.AuthenticationState.UNAUTHENTICATED -> {
+                    startActivity(Intent(this, AuthenticationActivity::class.java))
+                }
+                else -> Log.e(
+                    "warning",
+                    "Authentication state that doesn't require any UI change $authenticationState"
+                )
+            }
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
